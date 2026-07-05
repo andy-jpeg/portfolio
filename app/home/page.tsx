9e0374ce43a4d7e4e41035c7e995c6ddae5a74a2
andy-jpeg <156564624+andy-jpeg@users.noreply.github.com>
2026-07-05 01:41:38 -0400

    import background & apple kid font

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,9 +6,14 @@ export default function HomePage() {
         backgroundImage: "url('/themes/father3/background.png')",
         backgroundRepeat: "repeat",
         backgroundSize: "120px",
+        fontFamily: "Apple Kid, sans-serif",
       }}
     >
-      <div className="mx-auto flex min-h-screen w-full max-w-6xl items-center justify-center px-6 py-12 sm:px-8 lg:px-12"></div>
+      <div className="mx-auto flex min-h-screen w-full max-w-6xl items-center justify-center px-6 py-12 sm:px-8 lg:px-12">
+        <h1 className="text-8xl">
+          Hello This Is Ness From The Hit Game Earthbound
+        </h1>
+      </div>
     </main>
   );
 }
